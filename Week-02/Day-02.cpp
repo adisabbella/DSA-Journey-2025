dd0421157fc1f077d91fbf526804fed580dd95bd
adisabbella <adisabbella2502@gmail.com>
2025-09-16 09:04:35 +0530

    Day 9: Number Theory Problem

---
+++ Week-02/Day-02.cpp
@@ -0,0 +1,39 @@
+// Replace Non-Coprime Numbers in Array
+// 16-09-2025
+// Difficulty: Hard (Leetcode - Hard)
+
+#include <bits/stdc++.h>
+#include<numeric>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++) {
+            int curr = nums[i];
+            while (!ans.empty() && gcd(curr, ans.back()) > 1) {
+                curr = lcm(curr, ans.back());
+                ans.pop_back();
+            }
+            ans.push_back(curr);
+        }
+        return ans;
+    }
+};
+
+
+int main() {
+    Solution sol;
+
+    vector<int> nums = {6, 4, 3}; 
+    vector<int> result = sol.replaceNonCoprimes(nums);
+
+    cout << "Result: ";
+    for (int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
